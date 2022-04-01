5c3c70202b8c52354e1bbff53a028255bf0069d6
Alexander Albert <alex.albert@vilkrig.com>
2022-04-01 13:26:51 +0200

    Update Themes

--- lib/src/themes/dark_theme.dart
+++ lib/src/themes/dark_theme.dart
@@ -6,3 +6,16 @@
  * Copyright (c) 2022 Vilkrig Hungary Kft. All rights reserved.
  * See LICENSE for distribution and usage details.
  */
+
+import 'package:flutter/material.dart';
+
+class ConnectaxLightTheme {
+  static ThemeData get dark {
+    return ThemeData(
+      brightness: Brightness.dark,
+      useMaterial3: true,
+      canvasColor: Colors.transparent,
+      scaffoldBackgroundColor: Colors.black,
+    );
+  }
+}
--- lib/src/themes/light_theme.dart
+++ lib/src/themes/light_theme.dart
@@ -6,3 +6,16 @@
  * Copyright (c) 2022 Vilkrig Hungary Kft. All rights reserved.
  * See LICENSE for distribution and usage details.
  */
+
+import 'package:flutter/material.dart';
+
+class ConnectaxDarkTheme {
+  static ThemeData get light {
+    return ThemeData(
+      brightness: Brightness.light,
+      useMaterial3: true,
+      canvasColor: Colors.transparent,
+      scaffoldBackgroundColor: Colors.white,
+    );
+  }
+}
