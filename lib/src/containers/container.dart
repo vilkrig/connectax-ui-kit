007f5db5b6e64b686d35879944a0fb76db2c5dc3
Alexander Albert <alex.albert@vilkrig.com>
2022-06-17 09:25:50 +0200

    Add Container

---
+++ lib/src/containers/container.dart
@@ -0,0 +1,19 @@
+/*
+ * Connectax UI Kit
+ * Created by Alexander Albert
+ * https://vilkrig.com
+ * 
+ * Copyright (c) 2022 Vilkrig Hungary Kft. All rights reserved.
+ * See LICENSE for distribution and usage details.
+ */
+
+import 'package:flutter/material.dart';
+
+class ConnectaxContainer extends StatelessWidget {
+  const ConnectaxContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
